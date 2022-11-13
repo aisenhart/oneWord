34d2c7ac3c43846d19492f784034087fd6d61483
aisenhart <andrew.isenhart@gmail.com>
2022-11-12 23:14:34 -0700

    First Commit!!

--- oneWord/ContentView.swift
+++ oneWord/ContentView.swift
@@ -9,13 +9,26 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+   
+        ZStack {
+            VStack {
+            Image("testImage").resizable().aspectRatio(contentMode: .fit).padding(.all)
+            
+                Text("Hi")
+                TextField("Enter Guess Here", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                    .padding(.all, 10.0)
+                    .background(Color.gray)
+                    .cornerRadius(/*@START_MENU_TOKEN@*/10.0/*@END_MENU_TOKEN@*/)
+                    
+            
         }
-        .padding()
+        
+        }
+       
+        
+        
+  
+        
     }
 }
 
