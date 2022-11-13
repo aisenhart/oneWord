4a28a0c892286fb201eb786816a4d2b9d805595e
aisenhart <andrew.isenhart@gmail.com>
2022-11-12 22:18:55 -0700

    Initial Commit

---
+++ oneWord/oneWordApp.swift
@@ -0,0 +1,17 @@
+//
+//  oneWordApp.swift
+//  oneWord
+//
+//  Created by Andrew Isenhart on 11/12/22.
+//
+
+import SwiftUI
+
+@main
+struct oneWordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
