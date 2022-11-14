b6588053a6a7560b23bc03c107c197727cfbd463
aisenhart <andrew.isenhart@gmail.com>
2022-11-13 20:46:24 -0600

    - starting new UI

--- oneWord/ContentView.swift
+++ oneWord/ContentView.swift
@@ -9,54 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-            VStack {
-                Divider()
-                Spacer()
-                    .frame(height: 30)
-                    .clipped()
-                RoundedRectangle(cornerRadius: 10, style: .continuous)
-                    .fill(.primary)
-                    .overlay {
-                        Image("testImage")
-                        .renderingMode(.original)
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .mask { RoundedRectangle(cornerRadius: 4, style: .continuous) }
-                        .padding(30)
-                        .background {
-                            Group {
-                            
-                        }
-                        }
-                    }
-                    .frame(width: 350, height: 350)
-                    .clipped()
-                Divider()
-                HStack {
-                    Image(systemName: "a.square")
-                        .imageScale(.large)
-                    Image(systemName: "a.square")
-                        .imageScale(.large)
-                    Image(systemName: "a.square")
-                        .imageScale(.large)
-                    Image(systemName: "a.square")
-                        .imageScale(.large)
-                    Image(systemName: "a.square")
-                        .imageScale(.large)
-                    Image(systemName: "a.square")
-                        .imageScale(.large)
-                    Image(systemName: "a.square")
-                        .imageScale(.large)
-                    Image(systemName: "a.square")
-                        .imageScale(.large)
-                    Image(systemName: "a.square")
-                        .imageScale(.large)
-                }
-                Divider()
-                RoundedRectangle(cornerRadius: 10, style: .continuous)
-                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottom)
-                    .clipped()
-            }
+            Text("hello world!")
         }
     }
 
