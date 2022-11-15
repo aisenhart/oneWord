390411c56fa700a15aa5d5ec40e8e4afc7096309
aisenhart <andrew.isenhart@gmail.com>
2022-11-14 18:27:01 -0600

    starting mainUI

--- oneWord/ContentView.swift
+++ oneWord/ContentView.swift
@@ -1,3 +1,4 @@
+
 //
 //  loginPage.swift
 //  oneWord
@@ -8,10 +9,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    //properties
+    var word: String = ""
+    
     var body: some View {
-            Text("hello world!")
+        ScrollView {
+            VStack {
+                RoundedRectangle(cornerRadius: 30, style: .continuous)
+                    .frame(height: 300)
+                    .clipped()
+                    .foregroundColor(Color(.secondarySystemFill))
+                    .overlay { Image("stickHanging")
+                            .renderingMode(.original)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            
+                        
+                    }
+                
+                Text(generateWord())
+                    .font(.largeTitle.weight(.medium))
+                Text("Let's get started.")
+                    .font(.title2.weight(.medium))
+                    .foregroundColor(.secondary)
+            }
+            .padding()
         }
     }
+    
+    
+    
+    
+    
+    
+    func generateWord() -> String {
+        let wordList = ["Hello", "World", "Swift", "iOS", "Xcode", "Mac", "Apple", "iPhone", "iPad", "MacBook", "MacBook Pro"]
+        let randomIndex = Int.random(in: 0..<wordList.count)
+        return wordList[randomIndex]
+    }
+    
+}
+
+
+
+
+
+
+
 
 
 struct ContentView_Previews: PreviewProvider {
@@ -19,3 +63,14 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
+
+
+
+
+
+
+
+
+
